8125ab7d7dd4a41c3832098e083291effef4e067
KadoProG <splatrain@gmail.com>
2023-09-02 15:13:55 +0900

    webiste縦画像のコンポーネント作成

--- components/website/vertical_descriptions.tsx
+++ components/website/vertical_descriptions.tsx
@@ -4,17 +4,55 @@ import { styled } from "styled-components";
 import VerticalDescription from "./vertical_description";
 
 const DivVerticalDescriptions = styled.div`
+  padding: 60px 0;
+  background: #eee;
+`;
+
+const DivVerticalDescription_container = styled.div`
+  background: #eee;
   display: flex;
+  max-width: 900px;
+  margin: 0 auto;
+  justify-content: space-between;
 `;
 
 const VerticalDescriptions = () => {
-  const arr = [1, 3, 3];
+  const arrData = [
+    {
+      title: "サンプルタイトル",
+      description: "こんにちは今回はタイピング練習をしていこうと思います。",
+      imageSrc: "/images/myself_2022-04-22.jpg",
+      imageAlt: "俺や",
+    },
+    {
+      title: "サンプルタイトル",
+      description: "こんにちは今回はタイピング練習をしていこうと思います。",
+      imageSrc: "/images/myself_2022-04-22.jpg",
+      imageAlt: "俺や",
+    },
+    {
+      title: "サンプルタイトル",
+      description: "こんにちは今回はタイピング練習をしていこうと思います。",
+      imageSrc: "/images/myself_2022-04-22.jpg",
+      imageAlt: "俺や",
+    },
+  ];
 
   return (
     <DivVerticalDescriptions>
-      {arr.map((num) => {
-        return <VerticalDescription key={num} />;
-      })}
+      <DivVerticalDescription_container>
+        {arrData.map((data, index) => {
+          return (
+            <VerticalDescription
+              title={data.title}
+              description={data.description}
+              imageSrc={data.imageSrc}
+              imageAlt={data.imageAlt}
+              key={index}
+            />
+          );
+        })}
+      </DivVerticalDescription_container>
     </DivVerticalDescriptions>
   );
 };
