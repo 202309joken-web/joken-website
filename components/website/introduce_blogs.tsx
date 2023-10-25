d2748e1528083e619a209138f7e4892f951d8c0f
KadoProG <splatrain@gmail.com>
2023-10-25 09:16:46 +0900

    add: HOMEの「活動日誌」作成

---
+++ components/website/introduce_blogs.tsx
@@ -0,0 +1,149 @@
+"use client";
+import styled from "styled-components";
+import Link from "next/link";
+
+const IntroduceBlogs = () => {
+  const blogData = [
+    {
+      date: "2023.10.25",
+      category: "活動報告",
+      color: "var(--color_theme_joken)",
+      title: "こんにちは今回はタイピング練習をしていこうと思います。",
+      link: "/",
+    },
+    {
+      date: "2023.10.25",
+      category: "活動報告",
+      color: "var(--color_theme_joken)",
+      title: "こんにちは今回はタイピング練習をしていこうと思います。",
+      link: "/",
+    },
+    {
+      date: "2023.10.25",
+      category: "活動報告",
+      color: "var(--color_theme_joken)",
+      title: "こんにちは今回はタイピング練習をしていこうと思います。",
+      link: "/",
+    },
+    {
+      date: "2023.10.25",
+      category: "活動報告",
+      color: "var(--color_theme_joken)",
+      title: "こんにちは今回はタイピング練習をしていこうと思います。",
+      link: "/",
+    },
+  ];
+  return (
+    <DivIntroduceBlogs>
+      <div>
+        <h2>
+          <span>
+            Activity <SpanBlue>L</SpanBlue>og
+          </span>
+          <span>
+            活動<SpanBlue>日</SpanBlue>誌
+          </span>
+        </h2>
+        <div>
+          {blogData.map((data, index) => {
+            return (
+              <DivItem $color={data.color} key={index}>
+                <div>
+                  <span className="date">{data.date}</span>
+                  <span className="category">{data.category}</span>
+                </div>
+                <p>
+                  <LinkA href={data.link}>{data.title}</LinkA>
+                </p>
+              </DivItem>
+            );
+          })}
+        </div>
+      </div>
+    </DivIntroduceBlogs>
+  );
+};
+
+export default IntroduceBlogs;
+
+const DivIntroduceBlogs = styled.div`
+  display: flex;
+  max-width: 1500px;
+  margin: 100px auto;
+  justify-content: center;
+  & > div {
+    width: 90%;
+    padding-bottom: 100px;
+    & h2 {
+      padding-bottom: 20px;
+      & > span {
+        display: block;
+        font-weight: initial;
+        text-align: center;
+        &:first-child {
+          font-size: 34px;
+        }
+        &:last-child {
+          font-size: 28px;
+        }
+      }
+    }
+    & > div {
+      border-top: 2px solid #ddd;
+    }
+  }
+`;
+
+// ここから、ブログリスト
+const DivItem = styled.div<{ $color: string }>`
+  padding: 20px 0%;
+  border-bottom: 2px solid #ddd;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  & p {
+    flex: 1;
+  }
+
+  & > div {
+    padding-right: 20px;
+    @media screen and (max-width: 467px) {
+      width: 110px;
+      padding-right: 0;
+    }
+    & > span {
+      display: inline-block;
+      width: 110px;
+      text-align: center;
+      &.category {
+        background: ${(props) => props.$color};
+        color: white;
+      }
+    }
+  }
+`;
+
+const SpanBlue = styled.span`
+  color: var(--color_theme_joken);
+`;
+
+const LinkA = styled(Link)`
+  color: #000;
+  font-family:
+    "Montserrat",
+    -apple-system,
+    BlinkMacSystemFont,
+    "Segoe UI",
+    Roboto,
+    "Helvetica Neue",
+    Arial,
+    sans-serif,
+    "Apple Color Emoji",
+    "Segoe UI Emoji",
+    "Segoe UI Symbol",
+    "Noto Color Emoji";
+
+  &:hover {
+    color: var(--color_theme_joken);
+  }
+`;
