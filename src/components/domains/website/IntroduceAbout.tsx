8e01af04e8c2758332348419f0f63ced65dfce19
KadoProG <splatrain@gmail.com>
2024-02-25 05:52:51 +0900

    デザインの修正

--- src/components/domains/website/IntroduceAbout.tsx
+++ src/components/domains/website/IntroduceAbout.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Paper, Typography } from '@mui/material';
 import Image from 'next/image';
 
 export const IntroduceAbout = () => (
@@ -8,20 +8,22 @@ export const IntroduceAbout = () => (
       borderTop={'6px solid #8ad3f2'}
       borderBottom={'14px solid #8ad3f2'}
       display={'flex'}
-      py={12}
+      py={18}
       px={'10%'}
       justifyContent={'center'}
       position={'relative'}
+      zIndex={1}
     >
-      <Box
+      <Paper
+        component={Box}
         id="about"
-        text-align={'center'}
+        textAlign={'center'}
         py={{ xs: 5, sm: 12 }}
         px={{ xs: 1, sm: 2 }}
-        bgcolor="#ecf0f8"
         maxWidth={800}
         position={'relative'}
         sx={{
+          bgcolor: '#ecf0f8',
           '&::before': {
             content: '""',
             position: 'absolute',
@@ -58,83 +60,17 @@ export const IntroduceAbout = () => (
           width={100}
           height={100}
           alt="Jokenしましま"
-          style={{ position: 'absolute', top: 40, right: 40, zIndex: -1 }}
+          style={{ position: 'absolute', top: -40, right: -40, zIndex: -1 }}
         />
-      </Box>
+      </Paper>
       <Image
         className="right"
         src="/images/joken_squid.png"
         width={120}
         height={120}
-        alt="Jokenしましま"
+        alt="Jokenイカ"
         style={{ position: 'absolute', bottom: 0, right: 0, zIndex: -1 }}
       />
     </Box>
   </Box>
 );
-
-// const DivIntroduceAbout = styled.div`
-//   display: flex;
-//   max-width: 1500px;
-//   margin: 100px auto;
-//   justify-content: center;
-
-//   & > div {
-//     width: 90%;
-//     border-top: 6px solid #8ad3f2;
-//     border-bottom: 14px solid #8ad3f2;
-//     display: flex;
-//     padding: 140px 10%;
-//     justify-content: center;
-//     position: relative;
-
-//     & div {
-//       text-align: center;
-//       padding: 100px 10px;
-//       background: #ecf0f8;
-//       max-width: 800px;
-//       position: relative;
-//       & h2 {
-//         font-size: 46px;
-//         & span {
-//           display: inline-block;
-//         }
-//       }
-
-//       @media screen and (max-width: 467px) {
-//         padding: 60px 10px;
-//         h2 {
-//           font-size: 26px;
-//         }
-//       }
-//       /* & p {
-//         font-size: 18px;
-//       } */
-
-//       & img.right {
-//         position: absolute;
-//         top: -40px;
-//         right: -40px;
-//         z-index: -1;
-//       }
-//       &::before {
-//         content: '';
-//         position: absolute;
-//         bottom: -40px;
-//         left: -40px;
-//         z-index: -1;
-//         max-width: 344px;
-//         max-height: 100%;
-//         width: 30vw;
-//         height: 30vw;
-//         background: #bed5e8;
-//       }
-//     }
-//     & img.right {
-//       position: absolute;
-//       bottom: 0%;
-//       right: 0;
-//       z-index: -1;
-//     }
-//   }
-// `;
